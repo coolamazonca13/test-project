889abf2da554b5991f0f3060c55571079d0276c3
Анастасия Леонова <94276178+coolamazonca13@users.noreply.github.com>
2021-11-14 21:23:58 +0600

    add gradle to repository

--- java-example/src/test/java/myFirstTest.java
+++ java-example/src/test/java/myFirstTest.java
@@ -1,17 +1,23 @@
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class myFirstTest {
-    private WebDriver driver;
-    private WebDriverWait wait;
+    private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @Test
     public void testOpenWindows() {
         driver= new ChromeDriver();
         wait= new WebDriverWait(driver, 10);
         driver.get("https://www.metro-cc.ru/");
+
+    }
+    @AfterAll
+    public static void stopDriver(){
+        System.out.println("End");
         driver.quit();
         driver= null;
     }
