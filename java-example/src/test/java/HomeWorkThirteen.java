775a39f1dd309bc0b747c9b928b00f417dab40e0
Анастасия Леонова <94276178+coolamazonca13@users.noreply.github.com>
2021-12-14 22:33:39 +0600

    Исправлено задание 13

--- java-example/src/test/java/HomeWorkThirteen.java
+++ java-example/src/test/java/HomeWorkThirteen.java
@@ -2,13 +2,11 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import sun.awt.windows.ThemeReader;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HomeWorkThirteen {
@@ -16,29 +14,33 @@ public class HomeWorkThirteen {
     private static WebDriverWait wait;
 
     @Test
-    public void testRegisterNewSale() {
+    public void testRegisterNewSale() throws InterruptedException {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
         driver.get(" http://localhost/litecart/en/");
 
-        driver.findElement(By.xpath("//a[@title='Purple Duck']")).click();
-        driver.findElement(By.name("add_cart_product")).click();
-        assert isElementPresent(By.xpath("//span[@class='quantity' and text()='1']"));
-        driver.findElement(By.xpath("//a[text()='Home']")).click();
-        driver.findElement(By.xpath("//a[@title='Blue Duck']")).click();
-        driver.findElement(By.name("add_cart_product")).click();
-        assert isElementPresent(By.xpath("//span[@class='quantity' and text()='2']"));
-        driver.findElement(By.xpath("//a[text()='Home']")).click();
-        driver.findElement(By.xpath("//a[@title='Green Duck']")).click();
-        driver.findElement(By.name("add_cart_product")).click();
-        assert isElementPresent(By.xpath("//span[@class='quantity' and text()='3']"));
+        for (int i = 1; i < 4; i++) {
+            driver.findElements(By.xpath("//ul[@class='listing-wrapper products']/li")).get(0).click();
+            if (isElementPresent(By.xpath("//select"))) {
+                selectFromDropDownItem("options[Size]", "Small");
+            }
+            driver.findElement(By.name("add_cart_product")).click();
+            assert isElementPresent(By.xpath("//span[@class='quantity' and text()='" + i + "']"));
+            driver.findElement(By.xpath("//a[text()='Home']")).click();
+        }
         driver.findElement(By.xpath("//a[contains(text(),'Checkout')]")).click();
-        driver.findElements(By.xpath("//button[text()='Remove']")).get(0).click();
-        wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//td[@class='item']"),2));
-        driver.findElements(By.xpath("//button[text()='Remove']")).get(0).click();
-        wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//td[@class='item']"),1));
-        driver.findElements(By.xpath("//button[text()='Remove']")).get(0).click();
-        wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//td[@class='item']"),0));
+        int count = driver.findElements(By.xpath("//td[@class='item']")).size();
+        System.out.println("Before del count = " + count);
+        for (int i = count; i > 0; i--) {
+            System.out.println("count=" + i);
+            driver.findElements(By.xpath("//button[text()='Remove']")).get(0).click();
+            wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//td[@class='item']"), i));
+        }
+    }
+
+    private void selectFromDropDownItem(String selectName, String optionName) {
+        Select dropDown = new Select(driver.findElement(By.name(selectName)));
+        dropDown.selectByVisibleText(optionName);
     }
 
     boolean isElementPresent(By locator) {
@@ -48,10 +50,6 @@ public class HomeWorkThirteen {
         } finally {
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         }
-    }
-
-    private int getCountFromUi(By locator) {
-        return Integer.parseInt(driver.findElement(locator).getText());
     }
 
     @AfterAll
